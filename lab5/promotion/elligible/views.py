a9470028b51cbe0d0c5afc0e7ca1b1015fac41f1
Suchit <110873739+Suchit027@users.noreply.github.com>
2025-03-13 18:57:04 +0530

    question 2 of lab 5

---
+++ lab5/promotion/elligible/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from datetime import date
+
+# Create your views here.
+def elligible(request):
+    if request.method == 'POST':
+        empid = request.POST.get('emp')
+        doj = int(request.POST.get('doj')[: 4])
+        now = int(date.today().year)
+        if now - doj > 5:
+            return render(request, 'elligibility.html', {'promotion': 'yes'})
+        else:
+            return render(request, 'elligibility.html', {'promotion': 'no'})
+    else:
+        return render(request, 'elligibility.html', dict())

ae01db67104affcf0bcb345004e6713f0fd33d50
Suchit <110873739+Suchit027@users.noreply.github.com>
2025-03-14 00:01:50 +0530

    lab 6 q1

---
+++ lab6/calculator/calculator_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+def calculate(request):
+    if request.method == 'POST':
+        op = request.POST.get('op')
+        n1 = int(request.POST.get('n1'))
+        n2 = int(request.POST.get('n2'))
+        if op == 'add':
+            return render(request, 'calculate.html', {'ans': str(n1 + n2)})
+        elif op == 'sub':
+            return render(request, 'calculate.html', {'ans': str(n1 - n2)})
+        elif op == 'mul':
+            return render(request, 'calculate.html', {'ans': str(n1 * n2)})
+        elif op == 'div':
+            if n2 == 0:
+                return render(request, 'calculate.html', {'ans': 'error'})
+            return render(request, 'calculate.html', {'ans': str(n1 / n2)})
+        else:
+            return render(request, 'calculate.html', {'ans': 'error'})
+    return render(request, 'calculate.html', dict())

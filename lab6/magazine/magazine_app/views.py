3bd7ad6533717ba4e63057bfd0e2018d33c384f0
Suchit <110873739+Suchit027@users.noreply.github.com>
2025-03-15 00:15:33 +0530

    question 2 done

---
+++ lab6/magazine/magazine_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def getinput(request):
+    if request.method == 'POST':
+        img = request.POST.get('img')
+        bgcolor = request.POST.get('bgcolor')
+        fsize = request.POST.get('fsize')
+        color = request.POST.get('color')
+        text = request.POST.get('msg')
+        return render(request, 'magazine_app/display.html', {'img': img, 'bgcolor': bgcolor, 'fsize': fsize,
+                                                             'color': color, 'text': text})
+    return render(request, 'magazine_app/input.html')
+
+def display(request):
+    pass
